ea5c3872630b5407f9636f7ec71444ecaf2b8664
alfadormx <alfadormx@users.noreply.github.com>
2024-05-10 23:26:19 +0200

    fixed rgb action

--- plugin/TPLinkTapoPlugin.py
+++ plugin/TPLinkTapoPlugin.py
@@ -150,16 +150,16 @@ TP_PLUGIN_ACTIONS = {
         'type': "communicate",
         'tryInline': True,
         "doc": "Change Brightness",
-        'format': "Change $[1] to color [2]",
+        'format': "Change $[1] to color $[2]",
         'data': {
             'deviceList': {
-                'id': PLUGIN_ID + ".Actions.OnOffTrigger.Data.DeviceList",
+                'id': PLUGIN_ID + ".Actions.RGB.Data.DeviceList",
                 'type': "choice",
                 'label': "choice",
                 "valueChoices": []
             },
             'dataBright': {
-                'id': PLUGIN_ID + ".Actions.DataRGB",
+                'id': PLUGIN_ID + ".Actions.RGB.Data.RGB",
                 'type': "color",
                 'label': "color",
                 'default': "#000000FF"
